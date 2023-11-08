06fd5c804d260a43f8b132203de7c479ff0c50eb
Huthifa <hozyfa954@gmail.com>
2023-11-08 11:00:27 +0100

    Create an event

---
+++ Presentation tier/Cafe/Models/Event.cs
@@ -0,0 +1,8 @@
+﻿namespace Cafe.Models;
+
+public class Event
+{
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int EntertainerId { get; set; }
+}
