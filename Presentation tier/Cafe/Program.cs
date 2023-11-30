59424c2014ee9716e04ccdc09c2c898a519e577b
Huthifa <hozyfa954@gmail.com>
2023-11-30 22:13:23 +0100

    Login presentation-tier

--- Presentation tier/Cafe/Program.cs
+++ Presentation tier/Cafe/Program.cs
@@ -3,13 +3,25 @@ using System.Net.Http;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Cafe;
-using Microsoft.Extensions.DependencyInjection;
+using Cafe.Auth;
+using Cafe.Services;
+using Cafe.Services.Http;
+using Microsoft.AspNetCore.Components.Authorization;
 using Radzen;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+
+AuthorizationPolicies.AddPolicies(builder.Services);
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
 builder.Services.AddRadzenComponents();
+
 await builder.Build().RunAsync();
