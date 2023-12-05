7c9ab47a80f9de9bbbc95ddfb895efbd1f1b8823
D1e2N3i4M52003 <denim2003@abv.bg>
2023-12-05 13:32:46 +0200

    Moving classes

--- Presentation tier/Shared/Models/Booking.cs
+++ Presentation tier/Shared/Models/Booking.cs
@@ -1,4 +1,4 @@
-﻿namespace Cafe.Models;
+﻿namespace Shared.Models;
 
 public class Booking
 {
--- Presentation tier/Shared/Models/Event.cs
+++ Presentation tier/Shared/Models/Event.cs
@@ -1,6 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace Cafe.Models;
+namespace Shared.Models;
 
 public class Event
 {
